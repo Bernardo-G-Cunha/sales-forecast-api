a194fab9af67a87321af12d666b256e4e374042b
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2026-07-10 23:05:50 -0300

    feat: add aws variables

--- common/config.py
+++ common/config.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
@@ -6,3 +10,6 @@ DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 MODEL_DIR = PROJECT_ROOT / "models"
+
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+S3_BUCKET = os.getenv("S3_BUCKET")
